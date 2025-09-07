56ce9948f38431ed798c1d81c1da0462cc52c82e
Per-Olov Jernberg <possan@possan.se>
2025-09-08 00:38:27 +0200

    Use heap allocator

--- components/ctagSoundProcessor/ctagSPAllocator.cpp
+++ components/ctagSoundProcessor/ctagSPAllocator.cpp
@@ -61,8 +61,8 @@ void *ctagSPAllocator::Allocate(std::size_t const &size) {
     void *ptr = nullptr;
 
     ESP_LOGI("ctagSPAllocator", "Allocate: allocating %d bytes", size);
-    // ptr = heap_caps_calloc(1, size, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
-    ptr = malloc(size);
+    ptr = heap_caps_calloc(1, size+256, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT | MALLOC_CAP_EXEC);
+    // ptr = malloc(size + 256);
     ESP_LOGI("ctagSPAllocator", "Allocate: ptr=0x%"PRIu32, (uint32_t)ptr);
 
     // ptr = heap_caps_calloc(1, size, MALLOC_CAP_32BIT | MALLOC_CAP_SPIRAM);
