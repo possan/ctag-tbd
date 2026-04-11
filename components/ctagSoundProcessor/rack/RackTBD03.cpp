2cdb043d104952acb29e91b47c6bcf7724775a2b
Per-Olov Jernberg <possan@possan.se>
2026-04-11 21:45:49 +0200

    More fixes

--- components/ctagSoundProcessor/rack/RackTBD03.cpp
+++ components/ctagSoundProcessor/rack/RackTBD03.cpp
@@ -95,7 +95,6 @@ void RackTBD03::Process(const PicoSeqRackProcessData &data) {
     // }
 
     if (midi_trig && !td3_pre_trig) {
-        // printf("TBDD3\n");
         td3_isAccent = td3_accent;
         // if (trig_td3_accent != -1) {
         //     td3_isAccent = data.trig[trig_td3_accent] == 0 ? 1 : 0;
@@ -121,6 +120,9 @@ void RackTBD03::Process(const PicoSeqRackProcessData &data) {
         td3_osc.Strike();
         td3_pre_trig = true;
         midi_trig = false;
+
+        printf("TBDD3 dvcf=%1.3f dvca=%1.3f note=%d\n", dvcf, dvca, midi_note);
+
     } else if (!midi_trig) {
         td3_pre_trig = false;
     }
